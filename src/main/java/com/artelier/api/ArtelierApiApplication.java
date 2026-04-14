9118486102942b0d7bec8961834107c363c88fb7
MimiRandomS <geronimo0scorpion@gmail.com>
2026-04-13 20:58:38 -0500

    first commit

---
+++ src/main/java/com/artelier/api/ArtelierApiApplication.java
@@ -0,0 +1,13 @@
+package com.artelier.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtelierApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtelierApiApplication.class, args);
+	}
+
+}
