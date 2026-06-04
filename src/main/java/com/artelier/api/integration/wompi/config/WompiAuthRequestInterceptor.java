4e186063f2e3c7e36242920f22a5882bc209c8ba
MimiRandomS <geronimo0scorpion@gmail.com>
2026-06-04 13:49:29 -0500

    fix(wompi): remove ObjectMapper injection from Feign config context

--- src/main/java/com/artelier/api/integration/wompi/config/WompiAuthRequestInterceptor.java
+++ src/main/java/com/artelier/api/integration/wompi/config/WompiAuthRequestInterceptor.java
@@ -6,7 +6,9 @@ import feign.RequestTemplate;
 import feign.codec.ErrorDecoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class WompiAuthRequestInterceptor implements RequestInterceptor {
 
     @Value("${wompi.private-key}")
@@ -18,7 +20,7 @@ public class WompiAuthRequestInterceptor implements RequestInterceptor {
     }
 
     @Bean
-    public ErrorDecoder wompiErrorDecoder(ObjectMapper objectMapper) {
-        return new WompiErrorDecoder(objectMapper);
+    public ErrorDecoder wompiErrorDecoder() {
+        return new WompiErrorDecoder(new ObjectMapper());
     }
 }
